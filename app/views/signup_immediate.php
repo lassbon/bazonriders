d0ba17ff0239f2ee4a5b0981155f5157273ac48e
Abayomi <roshbon@gmail>
2018-04-12 12:15:41 +0100

    started working on the tabs for the signup

---
+++ app/views/signup_immediate.php
@@ -0,0 +1,15 @@
+
+<div class="row">
+
+  <div class="col-sm-12">
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Menu 1</a></li>
+        <li><a href="#">Menu 2</a></li>
+    </ul>
+  </div>
+
+
+</div>
+
+
